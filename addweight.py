8d04b4cf6d8fcc957599edd6887480499d1a92eb
YangGaru <a15894789@gmail.com>
2021-07-27 13:45:15 +0800

    改

--- addweight.py
+++ addweight.py
@@ -5,7 +5,7 @@ img1 = cv2.imread('./images/test1.jpg')
 print(img1.shape)
 roi1 = img1[400:600,300:700]
 roi2 = img1[100:300,800:1200]
-dst = cv2.addWeighted(roi1,0.1,roi2,0.1,0)
+dst = cv2.addWeighted(roi1,0.7,roi2,0.3,0)
 img1[100:300,800:1200] = dst
 #秀圖
 cv2.namedWindow('test',cv2.WINDOW_NORMAL)
