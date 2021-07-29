ff9a2210d3dbe3be76f6b3503de06243baa8e852
YangGaru <a15894789@gmail.com>
2021-07-29 17:42:29 +0800

    fft & template 練習

--- hist2.py
+++ hist2.py
@@ -2,18 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('./images/test1.jpg', 0)
-# 别忘了中括号 [img],[0],None,[256],[0,256]，只有 mask 没有中括号
-hist1 = cv2.calcHist([img1], [0], None, [256], [0, 256])
+img = cv2.imread('./images/test1.jpg')
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
 
-mask = np.zeros(img1.shape[:2], np.uint8)
-mask[100:600, 400:800] = 255
-masked_img = cv2.bitwise_and(img1,img1,mask = mask)
-hist_full = cv2.calcHist([masked_img],[0],None,[256],[0,256])
-hist_mask = cv2.calcHist([img1],[0],mask,[256],[0,256])
+plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB)), plt.axis('off'),
+plt.title('Original')
 
-plt.subplot(221), plt.imshow(img1, 'gray'), plt.title('Image1')
-plt.subplot(222), plt.plot(hist1),plt.title('Histogram'), plt.xlim([0, 256])
-plt.subplot(223), plt.imshow(masked_img, 'gray' ), plt.title('Image2')
-plt.subplot(224), plt.plot(hist_full),plt.plot(hist_mask),plt.title('Histogram'), plt.xlim([0, 256])
+plt.subplot(122), plt.imshow(hist,'gray'),
+plt.title('2D Histogram')
+
 plt.show()
